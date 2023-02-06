747d2fb77a1d0a90ae0b09ed9260bc90478c4f19
Noah <noahellisk@gmail.com>
2023-02-05 18:22:35 -0600

    Adding knight and pawn moves

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ mod moves;
 mod utility;
 mod pieces;
 
-fn main() {
-    // let game = Board::new();
-    // game.print();
+use crate::pieces::pawn;
 
+fn main() {
     let w_pawns: u64 = 0x000000000102FC00;
     let empty: u64 = 0xFFFFFFFFFFFFFFFF;
-    let moveset: u64 = pieces::w_single_push_pawns(w_pawns, empty);
+    let moveset: u64 = pawn::w_single_push_squares(w_pawns, empty);
     utility::print_as_board(w_pawns);
     utility::print_as_board(moveset);
-    let moveset: u64 = pieces::w_double_push_pawns(w_pawns, empty);
+    let moveset: u64 = pawn::w_double_push_squares(w_pawns, empty);
     utility::print_as_board(moveset);
+    for i in 0..64 {
+        utility::print_as_board(pieces::knight::KNIGHT_MOVES[i]);
+    }
 }
